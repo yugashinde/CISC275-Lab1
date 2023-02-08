73279ea5e34fef229340d2a78f76059edf0ceafc
Donovan Messer <89869979+donomess@users.noreply.github.com>
2023-02-08 12:29:17 -0500

    Added file for students to change.

---
+++ basicinfo.tsx
@@ -0,0 +1,13 @@
+/* This is a basic file we are using to familiarize ourselves with you! Simply un-comment these
+below variables and update them with stuff about you. Yeah... I know... the classic basic info
+everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
+
+/*Un-comment this and fill in the info
+const yourName : String = "Your name";
+let yourPronouns : String = "Pro/Nouns";
+const yourMajor : String = "Your major";
+let classification : String = "freshman/soph/junior/senior/supersenior";
+let oneExcite : String = "One thing you're excited about in this course";
+let anyConcerns : String = "If anything, list what you may be worried about in this course";
+let funFact : String = "And the classic, list one fun fact about yourself";
+*/
